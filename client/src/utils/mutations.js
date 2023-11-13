c51fb62d626cefc72681ab79048754deeb89e0f1
Caitlin Ash <caitlinba27@gmail.com>
2023-11-13 15:22:07 -0600

    Fixed bugs with site not displaying

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -13,7 +13,7 @@ export const LOGIN_USER = gql`
 `;
 
 export const ADD_USER = gql`
-  mutation addUser($username: String!, $email: String!, password: String!) {
+  mutation addUser($username: String!, $email: String!, $password: String!) {
     login(username: $username, email: $email, password: $password) {
         token
         user {
