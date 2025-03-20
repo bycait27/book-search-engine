34a23c0a099e1c1d6f41df48b9360115eb1dbb36
bycait27 <caitlinba27@gmail.com>
2025-03-20 14:05:42 -0500

    Deploying to vercel

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,12 @@ import reactRefresh from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [reactRefresh()],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true,
+    sourcemap: false
+  },
   server: {
     port: 3000,
     open: true,
